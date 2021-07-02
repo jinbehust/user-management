1a6a894b8b4ff57774805714b640e210f2f7571f
Giang Nguyen Duc <ducgiangbkhn@gmail.com>
2021-07-02 13:47:30 +0700

    add helmet and snyk

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
+
 require('dotenv').config();
 const errorHandler = require('./_middlewares/error-handler');
 const userRouter = require('./src/routes/user');
@@ -10,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
